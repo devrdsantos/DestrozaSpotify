7379c79599cda3ce42c17a5a3283e4dde6667645
Izanrb <izan.rodriguezba@elorrieta-errekamari.com>
2024-04-25 21:23:51 +0200

    [ADD] Componentes extras

--- DestrozaSpotify/src/panel/PanelGestionMusica.java
+++ DestrozaSpotify/src/panel/PanelGestionMusica.java
@@ -24,11 +24,23 @@ public class PanelGestionMusica extends JPanel{
 	private static final long serialVersionUID = 1L;
 
 	private GestionBD gestionBD;
-	private JTextField textFieldNombreCancion;
 	private JTextField textFieldDuracion;
 	private JTextField textFieldPortada;
 	private JTextField textFieldColaboradores;
-	private JTextField textField;
+	private JTextField textFieldAudio;
+	private JTextField textFieldNombreCancion;
+	private JLabel lblNombreAudio;
+	private JLabel lblDescripcion;
+	private JLabel lblPortada;
+	private JLabel lblColaboradores;
+	private JLabel lblAudio;
+	private JLabel lblArtista;
+	private JLabel lblAlbum;
+	private JComboBox comboBoxAlbum;
+	private JComboBox comboBoxArtistas;
+	private JButton btnSubirPortada;
+	private JButton btnSubirWav;
+	private JLabel lblNombreAudioE;
 	
 	public PanelGestionMusica(VistaPrincipal v) {
 		gestionBD = new GestionBD();
@@ -75,6 +87,26 @@ public class PanelGestionMusica extends JPanel{
 		MenuBotones.add(btnModificarAlbum);
 		
 		JButton btnAñadirCancion = new JButton("Añadir cancion");
+		btnAñadirCancion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblArtista.setVisible(true);
+				lblAlbum.setVisible(true);
+				lblAudio.setVisible(true);
+				lblColaboradores.setVisible(true);
+				lblDescripcion.setVisible(true);
+				lblNombreAudio.setVisible(true);
+				lblPortada.setVisible(true);
+				textFieldAudio.setVisible(true);
+				textFieldColaboradores.setVisible(true);
+				textFieldDuracion.setVisible(true);
+				textFieldNombreCancion.setVisible(true);
+				textFieldPortada.setVisible(true);
+				btnSubirPortada.setVisible(true);
+				btnSubirWav.setVisible(true);
+				comboBoxAlbum.setVisible(true);
+				comboBoxArtistas.setVisible(true);
+			}
+		});
 		btnAñadirCancion.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnAñadirCancion.setBounds(10, 90, 150, 30);
 		MenuBotones.add(btnAñadirCancion);
@@ -85,6 +117,11 @@ public class PanelGestionMusica extends JPanel{
 		MenuBotones.add(btnModificarCancion);
 		
 		JButton btnEliminarCancion = new JButton("Eliminar cancion");
+		btnEliminarCancion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnEliminarCancion.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnEliminarCancion.setBounds(10, 210, 150, 30);
 		MenuBotones.add(btnEliminarCancion);
@@ -112,83 +149,113 @@ public class PanelGestionMusica extends JPanel{
 		lblMenuAdministrador.setBounds(360, 55, 480, 48);
 		add(lblMenuAdministrador);
 		
-		JLabel lblNombreAudio = new JLabel("Nombre de la cancion:\r\n");
+		lblNombreAudio = new JLabel("Nombre de la cancion:\r\n");
+		lblNombreAudio.setForeground(Color.decode("#ffffff"));
 		lblNombreAudio.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblNombreAudio.setBounds(270, 175, 211, 48);
+		lblNombreAudio.setVisible(false);
 		add(lblNombreAudio);
 		
-		JLabel lblDescripcion = new JLabel("Duracion (En segundos):");
+		lblDescripcion = new JLabel("Duracion (En segundos):");
+		lblDescripcion.setForeground(Color.decode("#ffffff"));
 		lblDescripcion.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblDescripcion.setBounds(253, 234, 228, 48);
+		lblDescripcion.setVisible(false);
 		add(lblDescripcion);
 		
 		textFieldNombreCancion = new JTextField();
 		textFieldNombreCancion.setBounds(510, 188, 288, 30);
 		add(textFieldNombreCancion);
+		textFieldNombreCancion.setVisible(false);
 		textFieldNombreCancion.setColumns(10);
 		
 		textFieldDuracion = new JTextField();
 		textFieldDuracion.setColumns(10);
 		textFieldDuracion.setBounds(510, 247, 288, 30);
+		textFieldDuracion.setVisible(false);
 		add(textFieldDuracion);
 		
-		JLabel lblPortada = new JLabel("Portada:");
+		lblPortada = new JLabel("Portada:");
+		lblPortada.setForeground(Color.decode("#ffffff"));
 		lblPortada.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblPortada.setBounds(396, 296, 85, 48);
+		lblPortada.setVisible(false);
 		add(lblPortada);
 		
 		textFieldPortada = new JTextField();
 		textFieldPortada.setColumns(10);
 		textFieldPortada.setBounds(510, 309, 288, 30);
+		textFieldPortada.setVisible(false);
 		add(textFieldPortada);
 		
-		JButton btnSubirPortada = new JButton("Subir .jpg");
+		btnSubirPortada = new JButton("Subir .jpg");
 		btnSubirPortada.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnSubirPortada.setBounds(823, 307, 111, 33);
+		btnSubirPortada.setVisible(false);
 		add(btnSubirPortada);
 		
-		JLabel lblColaboradores = new JLabel("Colaboradores:");
+		lblColaboradores = new JLabel("Colaboradores:");
+		lblColaboradores.setForeground(Color.decode("#ffffff"));
 		lblColaboradores.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblColaboradores.setBounds(333, 355, 148, 48);
+		lblColaboradores.setVisible(false);
 		add(lblColaboradores);
 		
 		textFieldColaboradores = new JTextField();
 		textFieldColaboradores.setColumns(10);
 		textFieldColaboradores.setBounds(510, 368, 288, 30);
+		textFieldColaboradores.setVisible(false);
 		add(textFieldColaboradores);
 		
-		JLabel lblArtista = new JLabel("Artista:");
+		lblArtista = new JLabel("Artista:");
+		lblArtista.setForeground(Color.decode("#ffffff"));
 		lblArtista.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblArtista.setBounds(406, 473, 75, 48);
+		lblArtista.setVisible(false);
 		add(lblArtista);
 		
-		JComboBox comboBoxArtistas = new JComboBox();
+		comboBoxArtistas = new JComboBox();
 		comboBoxArtistas.setBounds(510, 486, 288, 30);
+		comboBoxArtistas.setVisible(false);
 		add(comboBoxArtistas);
 		
-		JLabel lblAlbum = new JLabel("Album:");
+		lblAlbum = new JLabel("Album:");
+		lblAlbum.setForeground(Color.decode("#ffffff"));
 		lblAlbum.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblAlbum.setBounds(406, 532, 75, 48);
+		lblAlbum.setVisible(false);
 		add(lblAlbum);
 		
-		JComboBox comboBoxAlbum = new JComboBox();
+		comboBoxAlbum = new JComboBox();
 		comboBoxAlbum.setBounds(510, 545, 288, 30);
+		comboBoxAlbum.setVisible(false);
 		add(comboBoxAlbum);
 		
-		JLabel lblAudio = new JLabel("Audio:");
+		lblAudio = new JLabel("Audio:");
+		lblAudio.setForeground(Color.decode("#ffffff"));
 		lblAudio.setFont(new Font("Verdana", Font.PLAIN, 18));
 		lblAudio.setBounds(416, 414, 65, 48);
+		lblAudio.setVisible(false);
 		add(lblAudio);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(510, 424, 288, 30);
-		add(textField);
+		textFieldAudio = new JTextField();
+		textFieldAudio.setColumns(10);
+		textFieldAudio.setBounds(510, 424, 288, 30);
+		textFieldAudio.setVisible(false);
+		add(textFieldAudio);
 		
-		JButton btnSubirWav = new JButton("Subir .wav");
+		btnSubirWav = new JButton("Subir .wav");
 		btnSubirWav.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnSubirWav.setBounds(823, 423, 111, 33);
+		btnSubirWav.setBounds(823, 422, 111, 33);
+		btnSubirWav.setVisible(false);
 		add(btnSubirWav);
+		
+		lblNombreAudioE = new JLabel("Nombre de la cancion:\r\n");
+		lblNombreAudioE.setForeground(Color.decode("#ffffff"));
+		lblNombreAudioE.setFont(new Font("Verdana", Font.PLAIN, 18));
+		lblNombreAudioE.setBounds(270, 175, 211, 48);
+		lblNombreAudioE.setVisible(false);
+		add(lblNombreAudioE);
 		
 	}
 }
