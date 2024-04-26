5c251fb8d70de14dea88b1dd2992c7b1599cb8e4
Karina M <karinamonetti@gmail.com>
2024-04-26 17:55:23 +0200

    [ADD] JScrollPane en PanelDePrueba para mostrar Paneles de Artistas (SIN TERMINAR)

--- DestrozaSpotify/src/panel/PanelDePrueba.java
+++ DestrozaSpotify/src/panel/PanelDePrueba.java
@@ -2,6 +2,7 @@ package panel;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Panel;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -13,8 +14,11 @@ import javax.swing.JComboBox;
 import javax.swing.JSeparator;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelDePrueba extends JPanel {
 	
@@ -42,6 +46,7 @@ public class PanelDePrueba extends JPanel {
 	btnAtras.setBorderPainted(false);
 	add(btnAtras);
 	
+	// LBL - Titulo principal
 	JLabel lblTitulo = new JLabel("Descubrir Música");
 	lblTitulo.setBounds(591, 131, 524, 74);
 	lblTitulo.setForeground(new Color(255, 255, 255));
@@ -49,12 +54,16 @@ public class PanelDePrueba extends JPanel {
 	lblTitulo.setFont(new Font("Open Sans", Font.BOLD, 48));
 	add(lblTitulo);
 	
+	// BTN - Perfil
 	JButton btnPerfil = new JButton("Perfil");
 	btnPerfil.setBounds(1355, 32, 137, 52);
+
+		// Acción
 	btnPerfil.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 		}
 	});
+	
 	btnPerfil.setOpaque(true);
 	btnPerfil.setForeground(new Color(255, 170, 67));
 	btnPerfil.setFont(new Font("Open Sans", Font.BOLD, 16));
@@ -62,35 +71,80 @@ public class PanelDePrueba extends JPanel {
 	btnPerfil.setBackground(new Color(63, 61, 61));
 	add(btnPerfil);
 	
+	// COMBOBOX - Género
 	JComboBox comboBoxGeneros = new JComboBox();
-	comboBoxGeneros.setBounds(775, 272, 417, 33);
+	comboBoxGeneros.setBounds(775, 256, 417, 33);
 	add(comboBoxGeneros);
 	
 	JLabel lblArtistasXGenero = new JLabel("Artistas por Género:");
-	lblArtistasXGenero.setBounds(538, 265, 224, 41);
+	lblArtistasXGenero.setBounds(538, 249, 224, 41);
 	lblArtistasXGenero.setForeground(new Color(255, 255, 255));
 	lblArtistasXGenero.setHorizontalAlignment(SwingConstants.RIGHT);
 	lblArtistasXGenero.setFont(new Font("Open Sans", Font.PLAIN, 20));
 	add(lblArtistasXGenero);
 	
 	JLabel lblNewLabel = new JLabel("Resultados:");
-	lblNewLabel.setBounds(139, 395, 159, 22);
+	lblNewLabel.setBounds(115, 355, 159, 22);
 	lblNewLabel.setForeground(new Color(255, 255, 255));
 	lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 	lblNewLabel.setFont(new Font("Open Sans", Font.PLAIN, 18));
 	add(lblNewLabel);
 	
-	JLabel lblNewLabel_1 = new JLabel("New label");
-	lblNewLabel_1.setBounds(242, 203, 186, 171);
-	lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-	add(lblNewLabel_1);
+	// BTN - Resultado: Artista
+	JPanel panel = new JPanel();
+
+		// Acción
+	panel.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+	});
 	
-	JScrollPane scrollPane = new JScrollPane();
-	scrollPane.setBounds(173, 445, 1133, 413);
-	scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	scrollPane.setToolTipText("");
-	scrollPane.setLayout(null);
-	add(scrollPane);
+	panel.setBackground(Color.decode("#3f3d3d"));
+	panel.setBounds(146, 399, 280, 300);
+	add(panel);
+	panel.setLayout(null);
+	
+		// LBL - Nombre Artista
+	JLabel lblNombreArtista = new JLabel("<html>Nombre Artista</html>");
+	lblNombreArtista.setForeground(new Color(255, 255, 255));
+	lblNombreArtista.setHorizontalAlignment(SwingConstants.CENTER);
+	lblNombreArtista.setFont(new Font("Open Sans", Font.PLAIN, 14));
+	lblNombreArtista.setBounds(51, 237, 160, 38);
+	panel.add(lblNombreArtista);
+	
+		// LBL - IMG
+	JLabel lblImg = new JLabel("Soy una imagen");
+	lblImg.setBounds(41, 26, 200, 200);
+	panel.add(lblImg);
+	lblImg.setHorizontalAlignment(SwingConstants.CENTER);
+	lblImg.setBackground(Color.decode("#FFFFFF"));
+	lblImg.setOpaque(true);
+
+	
+	
+	
+
+	// PRUEBA ALEJANDRO
+	/*DefaultListModel<Panel> listModel = new DefaultListModel<>();
+	for (String elemento : elementos) {
+		if (true) // insertar condicion
+			listModel.addElement(elemento);
+	}
+	getContentPane().setLayout(null);
+
+	JList<String> jList = new JList<String>(listModel);
+
+	JPanel panel = new JPanel();
+	panel.setBounds(0, 200, 350, 200);
+	getContentPane().add(panel);
+
+	JScrollPane jScrollPane = new JScrollPane(jList);
+	jScrollPane.setPreferredSize(new Dimension(300, panel.getHeight()));
+	panel.add(jScrollPane);*/
+	
+	
+	
 
 }
 	}
